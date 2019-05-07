d41752cf7396fda6786fb0498e2b4225b6b5ac34
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-07 09:23:04 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,9 +15,9 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+                 
+shipping_manifest["oil paintings"]
+  
 end
 
 def adding
@@ -25,15 +25,10 @@ def adding
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
-                 }
-  # add 2 muskets to the shipping_manifest hash below
+  }
+  
+  shipping_manifest["muskets"]= 2
+  shipping_manifest["gun powder"]= 4
 
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
+return shipping_manifest
 end
